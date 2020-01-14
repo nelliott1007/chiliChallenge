00143fd5debd27b63f222c451b9851add3acb606
Nikki Elliott <nelliott1007@stu.d214.org>
2020-01-14 13:01:42 -0600

    test

--- chiliChallenge/ViewController.swift
+++ chiliChallenge/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SCLAlertView
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate, UITextFieldDelegate {
 
     let sectionTitles = ["Appetizers","Entrees","Sides", "Drinks"]
     var numberOfItems = 0
@@ -12,15 +12,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     @IBOutlet weak var chillis: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addtextField: UITextField!
+
+    @IBOutlet weak var segAdd: UISegmentedControl!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var headerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let Defaults = UserDefaults.standard
+        if let storedInfo = Defaults.array(forKey: addtextField.text!){
+            appetizers = storedInfo as! [String]
+        }
+        print(appetizers)
         chillis.image = UIImage(named: "ch")
         tableView.delegate = self
         tableView.dataSource = self
-      
+        addtextField.isHidden = true
     }
-    @IBOutlet weak var tableView: UITableView!
-    
+ 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+         appetizers.append(addtextField.text!)
+         tableView.reloadData()
+         addtextField.text = nil
+         let userDefaults = UserDefaults.standard
+            userDefaults.set(appetizers, forKey: addtextField.text!)
+         print(appetizers)
+         return true
+    }
     
     @IBAction func button(_ sender: Any)
     {
@@ -28,80 +48,128 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
    
     
+  
+    
     func alert()
     {
-        let alertViewResponder: SCLAlertViewResponder = SCLAlertView().showSuccess("Hello Customer", subTitle: "This is your order.")
-        SCLAlertView().showInfo("Important info", subTitle: "You did it")
+        let alertViewResponder: SCLAlertViewResponder = SCLAlertView().showSuccess("Hello User", subTitle: "This is your order.")
+        SCLAlertView().showInfo("Important info", subTitle: "Order Inputted")
     }
     
    
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat
       {
-          return 35
+          return 40
       }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
       
       func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
       {
-          let label = UILabel()
-          label.backgroundColor = .red
-          label.text = sectionTitles[section]
-          label.font = UIFont(name: "Times", size: 30)
+//
+//        if section == 0
+//               {
+//                 return b1
+//               }
+//              else if section == 1
+//               {
+//                   return headerButton
+//               }
+//                else if section == 2
+//               {
+//                   return b1
+//               }
+//                else if section == 3
+//               {
+//                   return headerButton
+//               }
+//               else
+//               {
+//                   return b1
+//               }
+        
+               let label = UILabel()
+               label.backgroundColor = .red
+               label.text = sectionTitles[section]
+               label.font = UIFont(name: "Times", size: 30)
           return label
+       
       }
-      
+    @IBAction func addition(_ sender: Any) {
+        
+    }
+    
       func numberOfSections(in tableView: UITableView) -> Int
       {
          
           return 4
       }
       
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
       {
-        if sectionTitles == appetizers
+        if section == 0
         {
             return appetizers.count
         }
-        else if sectionTitles == entrees
+       else if section == 1
         {
             return entrees.count
         }
-         else if sectionTitles == drinks
-        {
-            return drinks.count
-        }
-         else
+         else if section == 2
         {
             return sides.count
         }
+         else if section == 3
+        {
+            return drinks.count
+        }
+        else
+        {
+            return 1
+        }
         
     }
+    
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
       {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.imageView?.image = UIImage (named: "ch")
+        cell?.textLabel?.font = UIFont(name: "Times", size: 15)
+       
+        
         if indexPath.section == 0 {
                      item = appetizers[indexPath.row]
             cell?.textLabel?.text = item
                   }
        else if  indexPath.section == 1 {
             item = entrees[indexPath.row]
-                      cell?.textLabel?.text = item
+            cell?.textLabel?.text = item
         }
         else if  indexPath.section == 2 {
                 item = sides[indexPath.row]
-                          cell?.textLabel?.text = item
+            cell?.textLabel?.text = item
             }
         else if  indexPath.section == 3 {
                 item = drinks[indexPath.row]
-                          cell?.textLabel?.text = item
+            cell?.textLabel?.text = item
             }
         else {
             print ("cell")
         }
-       // cell?.detailTextLabel = "hi"
+      
 
         return cell!
       }
+    
+    @IBAction func barButton(_ sender: Any)
+      {
+       addtextField.isHidden = false
+      addtextField.placeholder = "add"
+      }
+     
+      
   
 }
 
---
+++ chiliChallenge/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  chiliChallenge
+//
+//  Created by period4 on 1/8/20.
+//  Copyright © 2020 period4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
